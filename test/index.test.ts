5badaaf537a482b70ceea1a89d332db821f5ec63
2nthony <ant.hu@outlook.com>
2022-07-28 02:02:16 +0800

    test: update

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,9 @@
 import { test, expect } from "vitest";
 
-import { sum } from "../index";
+import { replaceString } from "../index";
 
 test("sync function from native code", () => {
-  expect(sum(1, 2)).toBe(3);
+  const str = "hello word!";
+
+  expect(replaceString(str, "word", "world")).toBe("hello world!");
 });
